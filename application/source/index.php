f27a824163583e375c903732d1f95cefd28e6c83
Greg Creutz <gregory.creutz@gmail.com>
2023-01-30 12:49:36 +0100

    add php

--- application/source/index.php
+++ application/source/index.php
@@ -1,5 +1,5 @@
 <?php
-  $result = ""; // déclaration de la variable $result en tant que chaîne vide
+  $result = "";
   if(isset($_GET['submit'])) {
     $from_currency = $_GET['from_currency'];
     $to_currency = $_GET['to_currency'];
@@ -17,40 +17,44 @@
   
     // Determine exchange rate based on from and to currencies
     $exchange_rate_key = $from_currency . '_' . $to_currency;
-    $exchange_rate = $exchange_rates[$exchange_rate_key];
-  
-    // Calculate result
-    $result = $amount * $exchange_rate;
+    
+    if(array_key_exists($exchange_rate_key, $exchange_rates)) {
+      $exchange_rate = $exchange_rates[$exchange_rate_key];
+      $result = $amount * $exchange_rate;
+    }
+    else {
+      $result = "Invalid currency conversion.";
+    }
   }
 
-  if(array_key_exists($exchange_rate_key, $exchange_rates)) {
-	$exchange_rate = $exchange_rates[$exchange_rate_key];
-	$result = $amount * $exchange_rate;
+  if (isset($_GET['switch'])) {
+	$tmp = $_GET['from_currency'];
+	$_GET['from_currency'] = $_GET['to_currency'];
+	$_GET['to_currency'] = $tmp;
   }
-  else {
-	$result = "Invalid currency conversion.";
-  }
+  
 ?>
 
-<form action="index.php" method="get">
-        <label for="from_currency">From :</label>
-        <select name="from_currency" id="from_currency">
-          <option value="euro">Euro</option>
-          <option value="dollar">Dollar</option>
-          <option value="pound">Pound</option>
-        </select>
-      
-        <label for="to_currency">To :</label>
-        <select name="to_currency" id="to_currency">
-          <option value="euro">Euro</option>
-          <option value="dollar">Dollar</option>
-          <option value="pound">Pound</option>
-        </select>
-      
-        <label for="amount">Amount:</label>
-        <input type="text" name="amount" id="amount">
-      
-        <input type="submit" value="Submit" name="submit">
-        <h2>Result: <?php echo $result; ?></h2>
-</form>
 
+<form action="index.php" method="get">
+  <label for="from_currency">From :</label>
+  <select name="from_currency" id="from_currency">
+    <option value="euro" <?php if ($_GET['from_currency'] == "euro") echo "selected"; ?>>Euro</option>
+    <option value="dollar" <?php if ($_GET['from_currency'] == "dollar") echo "selected"; ?>>Dollar</option>
+    <option value="pound" <?php if ($_GET['from_currency'] == "pound") echo "selected"; ?>>Pound</option>
+  </select>
+
+  <label for="to_currency">To :</label>
+  <select name="to_currency" id="to_currency">
+    <option value="euro" <?php if ($_GET['to_currency'] == "euro") echo "selected"; ?>>Euro</option>
+    <option value="dollar" <?php if ($_GET['to_currency'] == "dollar") echo "selected"; ?>>Dollar</option>
+    <option value="pound" <?php if ($_GET['to_currency'] == "pound") echo "selected"; ?>>Pound</option>
+  </select>
+
+  <label for="amount">Amount:</label>
+  <input type="text" name="amount" id="amount" value="<?php if (isset($_GET['amount'])) echo $_GET['amount']; ?>">
+
+  <input type="submit" value="Submit" name="submit">
+  <input type="submit" value="Switch" name="switch">
+  <h2>Result: <?php echo $result; ?></h2>
+</form>
